9504c9908a4988f50a8216661bce2283a1ada340
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2026-02-24 16:35:11 +0400

    initial commit

---
+++ src/main/java/com/example/vibe_store/VibeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibe_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vibe_store/VibeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vibe_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
